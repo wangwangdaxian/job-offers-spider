29be38be4997c763badead1b1867e2c81e3974e2
ReggieTAT <32627085+ReggieTAT@users.noreply.github.com>
2019-04-10 15:19:45 +0800

    [add]add mysql module for data storage

--- spider/config.py
+++ spider/config.py
@@ -12,3 +12,13 @@ REDIS_PASSWORD = 'reggie'
 REDIS_KEY = 'zhiping'
 
 # Mysql数据库配置
+MYSQL_HOST = '127.0.0.1'
+MYSQL_PORT = '3306'
+MYSQL_USER = 'root'
+MYSQL_PASSWORD = 'root'
+MYSQL_DATABASE = 'spider'
+
+MYSQL_TABLE = 'job_offers'
+MYSQL_SCHEMA = 'job_title VARCHAR(255),wage VARCHAR(255),location VARCHAR(255),req_exp VARCHAR(255),req_degree ' \
+               'VARCHAR(255),company VARCHAR(255),industry VARCHAR(255),stage VARCHAR(255),scale VARCHAR(255),' \
+               'id CHAR(32) '
---
+++ spider/module/db/mymysql.py
@@ -0,0 +1,48 @@
+import pymysql
+
+from spider.config import MYSQL_HOST, MYSQL_USER, MYSQL_PASSWORD, MYSQL_PORT, MYSQL_DATABASE, MYSQL_TABLE, MYSQL_SCHEMA
+
+
+class MySQL:
+    def __init__(self, host=MYSQL_HOST, username=MYSQL_USER, password=MYSQL_PASSWORD, port=MYSQL_PORT,
+                 database=MYSQL_DATABASE):
+        """
+        MySQL初始化
+        :param host:
+        :param username:
+        :param password:
+        :param port:
+        :param database:
+        """
+        try:
+            self.db = pymysql.connect(host, username, password, database, charset='utf8', port=port)
+            self.cursor = self.db.cursor()
+            self.create()
+        except pymysql.MySQLError as e:
+            print(e.args)
+
+    def insert(self, data, table=MYSQL_TABLE):
+        """
+        插入数据
+        :param table:
+        :param data:
+        :return:
+        """
+        keys = ', '.join(data.keys())
+        values = ', '.join(['%s'] * len(data))
+        sql_query = 'insert into %s (%s) values (%s)' % (table, keys, values)
+        try:
+            self.cursor.execute(sql_query, tuple(data.values()))
+            self.db.commit()
+        except pymysql.MySQLError as e:
+            print(e.args)
+            self.db.rollback()
+
+    def create(self, schema=MYSQL_SCHEMA, table=MYSQL_TABLE):
+        sql_query = 'create table %s if not exists(%s)' % (table, schema)
+        try:
+            self.cursor.execute(sql_query)
+            self.db.commit()
+        except pymysql.MySQLError as e:
+            print(e.args)
+            self.db.rollback()
