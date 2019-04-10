39041d410229915b5e2c46add85094e53ac95666
ReggieTAT <32627085+ReggieTAT@users.noreply.github.com>
2019-04-10 15:33:44 +0800

    [add]add my request class

---
+++ spider/utils/myrequest.py
@@ -0,0 +1,22 @@
+from requests import Request
+
+
+class MyRequest(Request):
+    DEFAULT_HEADERS = {
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,'
+                  'application/signed-exchange;v=b3 ',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Accept-Language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7',
+        'Cache-Control': 'max-age=0',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                      'Chrome/73.0.3683.103 Safari/537.36 '
+    }
+
+    def __init__(self, url, callback, method='GET', headers=None, need_proxy=False, fail_time=0, timeout=10):
+        if headers is None:
+            headers = self.DEFAULT_HEADERS
+        Request.__init__(self, method, url, headers)
+        self.callback = callback
+        self.need_proxy = need_proxy
+        self.fail_time = fail_time
+        self.timeout = timeout
